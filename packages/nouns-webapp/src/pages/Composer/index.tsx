92195d37583666084aea254f85fb5f7d7cd2022c
Juan M. Hernandez <juansmail@gmail.com>
2022-09-02 14:57:06 -0500

    Fix upload trait no palette bug, move local buildSVG to util file, clean-up

--- packages/nouns-webapp/src/pages/Composer/index.tsx
+++ packages/nouns-webapp/src/pages/Composer/index.tsx
@@ -15,7 +15,8 @@ import {
 import Noun from '../../components/Noun';
 import Link from '../../components/Link';
 import { ImageData, getNounData, getRandomNounSeed } from '@nouns/assets';
-import { DecodedImage, EncodedImage, PNGCollectionEncoder } from '@nouns/sdk';
+import { EncodedImage, PNGCollectionEncoder } from '@nouns/sdk';
+import { buildSVG } from '../../utils/composables-svg-builder';
 
 import { INounSeed } from '../../wrappers/nounToken';
 import { default as ComposablesImageData } from '../../libs/image-data/image-data-composables.json';
@@ -26,84 +27,6 @@ import { Trans } from '@lingui/macro';
 
 // @ts-ignore
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-/* start SVG */
-
-const decodeImage = (image: string): DecodedImage => {
-  const data = image.replace(/^0x/, '');
-  const paletteIndex = parseInt(data.substring(0, 2), 16);
-  const bounds = {
-    top: parseInt(data.substring(2, 4), 16),
-    right: parseInt(data.substring(4, 6), 16),
-    bottom: parseInt(data.substring(6, 8), 16),
-    left: parseInt(data.substring(8, 10), 16),
-  };
-  const rects = data.substring(10);
-
-  return {
-    paletteIndex,
-    bounds,
-    rects:
-      rects
-        ?.match(/.{1,4}/g)
-        ?.map(rect => [parseInt(rect.substring(0, 2), 16), parseInt(rect.substring(2, 4), 16)]) ??
-      [],
-  };
-};
-
-const getRectLength = (currentX: number, drawLength: number, rightBound: number): number => {
-  const remainingPixelsInLine = rightBound - currentX;
-  return drawLength <= remainingPixelsInLine ? drawLength : remainingPixelsInLine;
-};
-
-const buildSVG = (
-  parts: { data: string, palette?: string[] }[],
-  paletteColors: string[],
-  bgColor: string,
-): string => {
-  const svgWithoutEndTag = parts.reduce((result, part) => {
-    const svgRects: string[] = [];
-
-    const { bounds, rects } = decodeImage(part.data);
-    
-    const localPalette = (part.palette) ? (part.palette) :  null;
-    
-    let currentX = bounds.left;
-    let currentY = bounds.top;
-
-    rects.forEach(draw => {
-      let [drawLength, colorIndex] = draw;
-      const hexColor = (localPalette) ? localPalette[colorIndex] : paletteColors[colorIndex];
-
-      let length = getRectLength(currentX, drawLength, bounds.right);
-      while (length > 0) {
-        // Do not push rect if transparent
-        if (colorIndex !== 0) {
-          svgRects.push(
-            `<rect width="${length * 10}" height="10" x="${currentX * 10}" y="${
-              currentY * 10
-            }" fill="#${hexColor}" />`,
-          );
-        }
-
-        currentX += length;
-        if (currentX === bounds.right) {
-          currentX = bounds.left;
-          currentY++;
-        }
-
-        drawLength -= length;
-        length = getRectLength(currentX, drawLength, bounds.right);
-      }
-    });
-    result += svgRects.join('');
-    return result;
-  }, `<svg width="320" height="320" viewBox="0 0 320 320" xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges"><rect width="100%" height="100%" fill="#${bgColor}" />`);
-
-  return `${svgWithoutEndTag}</svg>`;
-};
-
-/* end SVG */
 
 /* start drag and drop */
 
@@ -347,7 +270,7 @@ const ComposerPage = () => {
 	partsComposed[14] = itemsForeground[2];
 	partsComposed[15] = itemsForeground[3];
 	
-	const svg = buildSVG(partsComposed.filter(part => part != null), ImageData.palette, background);
+	const svg = buildSVG(partsComposed.filter(part => part != null), encoder.data.palette, background);
 	setNounSVG(svg);
 	
   	// eslint-disable-next-line react-hooks/exhaustive-deps
@@ -493,7 +416,7 @@ const ComposerPage = () => {
         "data": data,
       };
       const parts = [ part ];
-      const svg = buildSVG(parts, ImageData.palette, 'fff');      
+      const svg = buildSVG(parts, encoder.data.palette, 'fff');      
       
       items.unshift({filename, data, svg, id: filename, content: filename, palette: null});
 
@@ -636,6 +559,7 @@ const ComposerPage = () => {
 	            />
 	            {pendingTrait && (
 	              <>
+	              	<br />
 	                <Button onClick={() => uploadCustomTrait()} className={classes.primaryBtn}>
 	                  <Trans>Upload</Trans>
 	                </Button>
